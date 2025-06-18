16740eebeb913328f5bb8c605e0aeefded120e58
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2025-06-18 15:43:17 +0700

    add : constant sizes theme

---
+++ src/app/theme/siezs.ts
@@ -0,0 +1,21 @@
+export const Sizes = {
+
+  // PADDING
+  paddingButtonM : 12,
+  paddingButtonMH: 16,
+  paddingButtonMV: 12,
+
+  // RADIUS
+  radiusSm : 8,
+  radiusM : 12,
+  radiusL : 16,
+  radiusXL : 20,
+  radius2XL : 24,
+
+  //ICON SIZE
+  iconSm : 12,
+  iconM : 16,
+  iconL : 20,
+  iconXL : 24,
+  icon2XL : 28,
+}
