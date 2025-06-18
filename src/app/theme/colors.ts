7b75dac2ed784b3fbd67442b500fbbd7ad18d8fb
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2025-06-18 09:50:52 +0700

    add : color and metrics theme

---
+++ src/app/theme/colors.ts
@@ -0,0 +1,5 @@
+export const Colors = {
+  background: '#FFFFFF',
+  canvasBackground: '#F3F4F6',
+  canvasBorder: '#D1D5DB',
+} as const;
---
+++ src/app/theme/metrics.ts
@@ -0,0 +1,5 @@
+export const Metrics = {
+  canvasSide: 300,
+  borderRadius: 8,
+  borderWidth: 2,
+} as const;
